0f2065a6c53fe06587aeb1d417548a9db4af4fd9
Amrit Thapa <realamritthapa@gmail.com>
2023-12-01 16:18:37 -0800

    added comments to manager_controller

--- manager_controller.py
+++ manager_controller.py
@@ -11,11 +11,13 @@ class ManagerControl:
     def __init__(self):
         pass
 
+    # Converts data object into a dictionary
     def makeDict(self, dataObj):
         dataDict = dict(name=dataObj.name, Id=dataObj.Id, address=dataObj.address,
                         city=dataObj.city, state=dataObj.state, zipcode=dataObj.zipcode)
         return dataDict
 
+    # Adds a new provider to the database
     def addProvider(self, providerObj):
         providerDict = self.makeDict(providerObj)
         try:
@@ -24,8 +26,7 @@ class ManagerControl:
         except Exception as e:
             return e
 
-    # this obj will have things like name, number etc..
-
+    # Adds a new member to the database
     def addMember(self, memberObj):
         memberDict = self.makeDict(memberObj)
         try:
@@ -34,6 +35,8 @@ class ManagerControl:
         except Exception as e:
             return e
 
+    # Edits an existing member's information in the database.
+    # First deletes the old record and then adds the updated one
     def editMember(self, memberId, memberObj):
         memDict = self.makeDict(memberObj)
         try:
@@ -43,6 +46,8 @@ class ManagerControl:
         except Exception as e:
             return e
 
+    # Edits an existing provider's information in the database.
+    # First deletes the old record and then adds the updated one
     def editProvider(self, providerId, providerObj):
         provDict = self.makeDict(providerObj)
         try:
@@ -52,6 +57,7 @@ class ManagerControl:
         except Exception as e:
             return e
 
+    # Removes the provider from that database using their Id
     def removeProvider(self, providerId):
         try:
             db.deleteProvider(providerId)
@@ -59,6 +65,7 @@ class ManagerControl:
         except Exception as e:
             return e
 
+    # Removes the member from that database using their Id
     def removeMember(self, memberId):
         try:
             db.deleteMember(memberId)
@@ -66,6 +73,7 @@ class ManagerControl:
         except Exception as e:
             return e
 
+    # Retrieves specific information about a service based on its code.
     def getServiceInfo(self, serviceCode, info):
         providerDirectory = db.getJSONListOfDicts(db.provDirPath)
         for dic in providerDirectory:
@@ -73,6 +81,7 @@ class ManagerControl:
                 return dic[info]
         return None
 
+    # Retrives the name of a provider using their Id
     def getProviderName(self, providerId):
         providerReg = db.getJSONListOfDicts(db.provRegPath)
         for dic in providerReg:
@@ -80,6 +89,7 @@ class ManagerControl:
                 return dic['name']
         return None
 
+    # Retrives the name of a member using their Id
     def getMemberName(self, memberId):
         memberReg = db.getJSONListOfDicts(db.memRegPath)
         for dic in memberReg:
@@ -87,6 +97,7 @@ class ManagerControl:
                 return dic['name']
         return None
 
+    # Retrieves the most recent service record based on file dates in a directory.
     def getRecentWeek(self):
         files = []
         for file in os.listdir(serviceDirectory):
@@ -97,12 +108,14 @@ class ManagerControl:
                 return None
         return files[-1]
 
+    # Calculates the total fee for a list of services
     def getTotalFee(self, serviceList):
         fee = 0
         for service in serviceList:
             fee += service['fee']
         return fee
 
+    # Retrives a list of services for a particular member
     def getMemberService(self, servicePath,  memberId):
         serviceRecord = db.getJSONListOfDicts(servicePath)
         serviceList = []
@@ -118,6 +131,7 @@ class ManagerControl:
                     {"date": dateOfService, "providerName": providerName, "serviceName": serviceName})
         return serviceList
 
+    # Retrives a list of services provided by a particular provider
     def getProviderService(self, servicePath, providerId):
         serviceRecord = db.getJSONListOfDicts(servicePath)
         serviceList = []
@@ -132,14 +146,19 @@ class ManagerControl:
                 serviceList.append(dic)
         return serviceList
 
-    def createMemberReport(self):
+    # Generates a comprehensive report for the most recent week.
+    # It includes member and provider service details, along with Electronic Transfer Fund (ETF) information.
+    def createReport(self):
         servicePath = self.getRecentWeek()
         weekName = servicePath[25:]
         memRegList = db.getJSONListOfDicts(db.memRegPath)
         provRegList = db.getJSONListOfDicts(db.provRegPath)
+        etfList = []
         memberDirectoryPath = ''
         providersDirectoryPath = ''
         etfDirectoryPath = ''
+
+        # Loops through each member, retrieves their service details, and adds them to the member directory.
         for memdic in memRegList:
             serviceList = self.getMemberService(servicePath, memdic['Id'])
             if serviceList:
@@ -154,8 +173,12 @@ class ManagerControl:
                 provdic['totalConsults'] = len(serviceList)
                 provdic['totalFee'] = self.getTotalFee(serviceList)
                 self.addProvDict(provdic, weekName, providersDirectoryPath)
-                print(providersDirectoryPath)
+                etfDic = {
+                    'providerName': provdic['name'], 'providerId': provdic['Id'], 'amountToTransfer': provdic['totalFee']}
+                etfList.append(etfDic)
+        db.createJSONListFile(etfDirectoryPath, etfList)
 
+    # Creates new directories for storing weekly reports, including separate directories for members and providers.
     def createWeekDirectories(self):
         memberDirectoryPath = ' '
         providersDirectoryPath = ' '
@@ -177,8 +200,10 @@ class ManagerControl:
                         newDirectoryPath, "members")
                     providersDirectoryPath = os.path.join(
                         newDirectoryPath, "providers")
-        return memberDirectoryPath, providersDirectoryPath, newDirectoryPath
+        etfDirectoryPath = os.path.join(newDirectoryPath, "etf.json")
+        return memberDirectoryPath, providersDirectoryPath, etfDirectoryPath
 
+     # Adds a member's information as a JSON file in the member directory for the specified week.
     def addMemDict(self, memObject, weekName, memberDirectoryPath):
         if os.path.exists(memberDirectoryPath):
             memberFileName = memObject['name'] + '_' + weekName
@@ -186,19 +211,11 @@ class ManagerControl:
                 memberDirectoryPath, memberFileName)
             db.createJSONFile(newMemberPathName, memObject)
 
+    # Adds a provider's information as a JSON file in the provider directory for the specified week.
+
     def addProvDict(self, provObject, weekName, providersDirectoryPath):
         if os.path.exists(providersDirectoryPath):
             providerFileName = provObject['name'] + '_' + weekName
             newProviderPathName = os.path.join(
                 providersDirectoryPath, providerFileName)
             db.createJSONFile(newProviderPathName, provObject)
-
-    def viewMemberReport(memberFile):
-        return database.memberReport(memberId)
-
-    def viewProviderReport(providerFile):
-        return database.providerReport(providerId)
-
-
-manager = ManagerControl()
-manager.createMemberReport()
