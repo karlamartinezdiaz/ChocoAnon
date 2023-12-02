aa9f8c07f8a0c3b006d7ef7364b5d2b26cc31ee8
Karla Martinez <karlamartinez@Karlas-iMac.local>
2023-12-01 20:33:54 -0800

    OMG

--- test_data.py
+++ test_data.py
@@ -14,17 +14,3 @@ def test_BasicData():
     assert basic_data.address == "1234 street"
     assert basic_data.Id == 123456789
     assert basic_data.zipcode == 12345
-
-
-def test_ServiceData():
-    def test_ServiceData():
-        with patch('builtins.input', side_effect=["12-01-2023"]):
-            service_data = ServiceData(
-                "02:00:00", "12-01-2023", 123456789, 987654321, 900000000, "Hay Girl")
-        service_data.createServiceData()
-        assert service_data.time == "12-01-2023 02:00:00"
-        assert service_data.dateOfService == "12-01-2023"
-        assert service_data.provideNumber == 123456789
-        assert service_data.memberNumber == 987654321
-        assert service_data.serviceCode == 900000000
-        assert service_data.comments == "Hay Girl"
--- test_manager_controller.py
+++ test_manager_controller.py
@@ -12,25 +12,41 @@ class TestDataObject:
         self.zipcode = zipcode
 
 
-def manager_instance():
+@pytest.fixture
+def test_manager():
     return ManagerControl()
 
+# def test_manager_control():
+    # manager_instance = ManagerControl()
+    # assert manager_instance is not None
 
-def test_manager_control(manager_instance):
-    with pytest.raises(ValueError, match="Invalid member ID"):
-        manager_instance.removeMember(22244422)
 
-
-def test_MakeDict(manager_instance):
+def test_make_dict(test_manager: ManagerControl):
     test_object = TestDataObject(
-        name="Yo mama", Id=123456789, address="246 Sally St", city="Portland", state="OR", zipcode=97123)
-    result_dict = manager_instance.makeDict(test_object)
+        "Karla Martinez", 123456789, "246 Sally St", "Portland", "OR", "97123")
 
     expected_result = {
-        'name': 'Yo mama',
+        'name': 'Karla Martinez',
         'Id': 123456789,
         'address': '246 Sally St',
         'city': 'Portland',
         'state': 'OR',
         'zipcode': '97123'
     }
+
+    result_dict = test_manager.makeDict(test_object)
+    assert result_dict == expected_result
+
+#####################################
+
+
+def test_add_provider(test_manager: ManagerControl):
+    test_provider_info = TestDataObject(
+        "Provider", 1234, "246 Sally St", "Portland", "OR", "97123")
+
+    result_provider = test_manager.addProvider(test_provider_info)
+
+    test_provider_dict = test_manager.makeDict(result_provider)
+
+    assert test_manager.addProvider(
+        test_provider_dict) == "Provider Successfully Added"
